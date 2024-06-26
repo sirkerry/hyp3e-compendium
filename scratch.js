9832547af9e95aaec6051c949e6ca817707c70fb
shermad1 <dsherman@colasiss.com>
2024-06-26 18:52:26 -0500

    All data pack updates

---
+++ scratch.js
@@ -0,0 +1,362 @@
+// Treasure Class A
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.jaHOOcPSQejseaqy");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.kJPJAfUEnQcIVimv");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.VsVkCbadzUzkaoUz");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.YPNsLib63Ml7rdOF");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.3kZ0fE29OEjTrGxP");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.hdbm0extIXX4oDeg");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.zvU5kVijSegFbG5K");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.IhGwlUuCIuvgRiHB");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class B
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.ZcEDRiQ6jxWJtA0q");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.dAbdVdB29XNB8UR4");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.Sig0UJQzCghq32gF");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.btkAaJhE30S5YOFT");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.UCN8QiFfjvHTcHah");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.4IWBSgKUKS5CAOlx");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.LYG6xjowUDbel4H8");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.K6fchJYCJinXEn3R");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.dZsTujfErrTBGflG");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class C
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.EvYta5X2hj4mja9R");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.4lqdXBkfS3vQUS1N");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.z72tqD1mExZufLOo");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.4WGcPPsrU1C7VpeH");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.GawTL6oNdXE9jEey");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.kLnaKShEPp6HPPUt");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class D
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.84JjZVP62dmarumY");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.i6ilhHbITZ5ZmNd0");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.Zg17YbeNdmDD62nl");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.3Hz9yJzSfSDkX8d9");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.rAK92blFOnyoHsp9");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.IScpxEuMJZxxC1SV");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.h1TissKIOsp0oCAV");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.EqivWFPyq7m2Yemr");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class E
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.PAS6NRCl98k4E8TY");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.GJDc7WNVyI8nG3Sl");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.yZRprGMX2KU8x0UV");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.DDEYjyD9i55fkcrK");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.f0M3Jmpl2SJEBw0q");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.8eVxxI9jJhWJNDEb");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.bASJiOAndsyqULjw");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.rps7GVCi1Pb0Om2Y");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class F
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.NBZ5dZ0rQfzyYiss");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.FsEXriqQyoGBwNIN");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.2jt44K9GHlVbDNfG");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.gXHSKmEvcx1UPS24");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.IGxEJfqyubnFbrgy");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.JL3RX6FDYtfhNeCW");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.SQabINOC0rSR5E1y");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.2hqfAyfZbkOJFOuR");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.CtwnHcMSOSgcTChl");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class G
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.YXpzCXz8zJcFYPqY");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.jgYY3xDrnskC4NGK");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.TjKgHeoTCmbD8ulm");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.NcCotcYnhthdA3An");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.g68FGd0eLuMKX6CS");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.6ifNt1d0G0JpWG4U");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class H
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.82bWRYxPAQGVltGI");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.8SWmwC9Nt3SdpdIY");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.p7CdOGLXeWZGxml6");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.JZbLjip6OKDq1XZN");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.7gk5VDRxrfiXIMt2");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.2PGzmmCiyMas5Hbu");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.bqb7ewuhIn5tXXbL");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.z0uZNf0DnOhKihPX");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.Z9R60YsCb36a6bZf");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.ydxkhqiA6f0FCZ3N");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class I
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.6mzkDxD8zF06TbfR");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.hNjdV6O4iSD5CXSS");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.Kyp6HOgU18sBhywf");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.OVeadVR2jIPvDSCq");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class J
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.l5DfhyxJ7beYxZk2");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class K
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.Zbm02j68kHL5YGU2");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class L
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.rVSdpsmtNXlgftJa");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class M
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.xMGWgKzw2wBpZjrY");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class N
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.9rvj1FkLZjUk71nj");
+await table.draw({roll: true, displayChat: true});
+
+
+//Treasure Class O
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.UKJacRGpMYQwmzA5");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.XxpqXRHFDO7j08oe");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class P
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.boSxjjuk1yg0FCF8");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.Vhbiqa6JdlrDDKR5");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class Q
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.mvyeBy1w7Dx2MYCK");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class R
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.dlpI1xb5V9yMoZtk");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.leQbcHkhRyvp3hT3");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.DRhPt45U9Qh7kH7r");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.msnm8NiHBCTHOFl4");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class S
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.ElaTGBY9FD4n1pom");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class T
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.r8VOVqjQpB7Ova77");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class U
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.0pc2NZ2plKr1PgG2");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.RdpNOHm8jswPstct");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.dfsa961u6XiDrA6Y");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class V
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.qR4IjEk2H7w8M1gf");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class W
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.UiBlksHHRkPajD0W");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.IeJ2Ji42rWKJVmvW");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.fyICEV09EmWVXENE");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.L03vdAnmFrwdtpAm");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.WF9Jo2WtAwueynSf");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class X
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.MFeddssGcWAferTp");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.T9ICD9qby7H4N4qz");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class Y
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.7wdO8T8ySGODl1Gx");
+await table.draw({roll: true, displayChat: true});
+
+
+// Treasure Class Z
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.1LBk3VvqGJkC56bw");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.RgzAZsGN0nO6yzpW");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.9rSNKbOvzaTFVjzR");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.8YHSRz0yh7msoOZh");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.wwHdyFOI8u0Xw6gA");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.9aHzfTjub6CpGcQp");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.jsA6uwXlW23t8c10");
+await table.draw({roll: true, displayChat: true});
+
+const table = await fromUuid("Compendium.hyperborea-3e-compendium.treasure-classes.RollTable.FjtDFyzHfzw1357Z");
+await table.draw({roll: true, displayChat: true});
